119e60bbab6f8af0e2304c5f45ff907858d56c19
Brian Stoner <brian@duckduckgo.com>
2015-11-25 10:23:46 -0500

    loader template helper

---
+++ test/loader.js
@@ -0,0 +1,8 @@
+describe('Handlebars.helpers.loader', function() {
+
+    it('should return a div', function() {
+        var expected = '<div class="loader" style="background-image:url(\"/loader/blackx1.png\")"></div>';
+        expect(Handlebars.helpers.loader('black')).toEqual(expected);
+    });
+
+});
