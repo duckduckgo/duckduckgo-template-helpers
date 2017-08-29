f28821432fa0110c5a6796da2b6d9dc132e0e0b7
Bradley Braithwaite <bradley@duckduckgo.com>
2017-08-29 10:06:41 -0400

    make unit tests pass

--- test/formatSubtitle.js
+++ test/formatSubtitle.js
@@ -1,28 +1,19 @@
 describe('Handlebars.helpers.formatSubtitle', function() {
-
-    var tests = [
-        { input: 'test', output: 'test' },
-        { input: ['test', 'test'], output: 'test<span class="sep"></span>test' },
-        { input: {
-              href: 'http://duckduckgo.com',
-              className: 'link--test',
-              text: 'Hi I am a link'
-          },
-          output: '<a href="http://duckduckgo.com" class="link--test">Hi I am a link</a>'
-        },
-        { input: [
-              { href: 'http://yahoo.com', text: 'Yahoo' },
-              { href: 'http://nytimes.com', text: 'NYT' },
-              'Plain Text'
-          ],
-          output: '<a href="http://yahoo.com">Yahoo</a><span class="sep"></span><a href="http://nytimes.com">NYT</a><span class="sep"></span>Plain Text'
-        }
-    ];
-
-    it('should return the correct value', function() {
-        tests.forEach(function(test) {
-            expect(Handlebars.helpers.formatSubtitle(test.input)).toEqual(test.output);
-        });
+    it('should return %nbsp for falsy value', function() {
+        spyOn(DDG, 'exec_template');
+        expect(Handlebars.helpers.formatSubtitle()).toEqual("&nbsp;");
+        expect(DDG.exec_template).not.toHaveBeenCalled();
     });
 
+    it('should return call to subtitle template for a single component value', function() {
+        spyOn(DDG, 'exec_template').and.returnValue('bar');
+        expect(Handlebars.helpers.formatSubtitle('foo')).toEqual('bar');
+        expect(DDG.exec_template).toHaveBeenCalledWith('subtitle', { components: ['foo'] });
+    });
+    
+    it('should return call subtitle template for an array of component values', function() {
+        spyOn(DDG, 'exec_template').and.returnValue('bar');
+        expect(Handlebars.helpers.formatSubtitle([1, 2, 3])).toEqual('bar');
+        expect(DDG.exec_template).toHaveBeenCalledWith('subtitle', { components: [1, 2, 3] });
+    });
 });
