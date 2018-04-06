54332dbc117f7338be6576e74febfba3f9e04963
Brian Stoner <brian@duckduckgo.com>
2018-04-06 08:32:54 -0400

    add option to only show icon in more at on mobile

--- template_helpers.js
+++ template_helpers.js
@@ -617,7 +617,7 @@
             meta.sourceIconUrl = '/assets/icon_favicon_placeholder.v104.png';
             meta.sourceIcon = false;
         }
-        if (!meta.hideMoreAtText && !ops.hideMoreAtText && !ops.dynamicMoreAtText && !(DDG.device.isMobile && ops.sourceOnlyMobile)) {
+        if (!meta.hideMoreAtText && !ops.hideMoreAtText && !ops.dynamicMoreAtText && !(DDG.device.isMobile && ops.sourceOnlyMobile) && !(DDG.device.isMobile && meta.iconOnlyOnMobile)) {
             meta.moreAtText = DDG.Text.MORE_AT_STRING;
         }
         // hides the 'More At' text when the source name is longer than the # of characters passed into the variable:
