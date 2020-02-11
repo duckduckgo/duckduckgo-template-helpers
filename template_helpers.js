93e052278afcb2435604b6f8a97a492e5c8b8691
Bradley Braithwaite <bradley@duckduckgo.com>
2020-02-11 18:59:01 -0500

    enable the passing of an object via handlebars

--- template_helpers.js
+++ template_helpers.js
@@ -384,8 +384,8 @@
      *
      */
 
-    Handlebars.registerHelper("imageProxy", function(url) {
-        return DDG.getImageProxyURL(url);
+    Handlebars.registerHelper("imageProxy", function(url, ops) {
+        return DDG.getImageProxyURL(url, ops);
     });
 
     /**
